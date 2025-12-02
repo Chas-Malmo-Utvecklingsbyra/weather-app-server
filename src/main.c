92e31033d4dde4962f0b3e2e51cf98dc0b520940
samstalhandske <samstalhandske@hotmail.com>
2025-12-02 13:40:43 +0100

    Updated server.

--- src/main.c
+++ src/main.c
@@ -1,194 +1,140 @@
 #include <stdio.h>
-
-#include "core/tcp/server/tcp_server.h"
+#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+#include <assert.h>
 
-#include "core/http/parser.h"
-#include "core/weather/weather.h"
-#include "core/http/http.h"
-#include "core/config/config.h"
-#include "server_routes/server_routes.h"
+#include "core/http/server/http_server.h"
+#include "core/utils/file.h"
 
-void on_received_bytes_from_client(TCP_Server *server, TCP_Server_Client *client, const uint8_t *buffer, const uint32_t buffer_size) {
-    (void)server; // TODO: SS - Remove these later.
-    (void)client; // TODO: SS - Remove these later.
+#include "core/weather/weather.h" // NOTE: SS -Should not be in Core.
 
-    /* TODO: SS - Try to parse the contents of the request buffer as a HTTP-request. */
-    printf("Received %u bytes from client:\n", buffer_size);
+bool route_root(HTTP_Method method, const HTTP_Query_Map *query_map, char *out_response, uint32_t response_max_buffer_size) {
+    (void)method; // NOTE: SS - We can check that 'method' is GET.
+    (void)query_map;
 
-    printf("'");
-    uint32_t i = 0;
-    for(; i < buffer_size; i++) {
-        printf("%c", *(buffer + i));
+    uint32_t data_length = 0; // NOTE: SS - Not used at the moment.
+    char *data = file_open_read_only("index.html", &data_length);
+    if(data != NULL) {
+        snprintf(&out_response[0], response_max_buffer_size, "%s", data); // NOTE: SS - This will most likely not work for binary.
     }
-    printf("'\n");
-    char *response_string = NULL;
+    else {
+        snprintf(&out_response[0], response_max_buffer_size, "<h1></h1>");
+    }
+
+    return true;
+}
+
+bool route_weather(HTTP_Method method, const HTTP_Query_Map *query_map, char *out_response, uint32_t response_max_buffer_size) {
+    (void)method; // NOTE: SS - We can check that 'method' is GET.
+
+    double latitude = 0; {
+        const char *str = NULL;
+        if(http_query_map_get(query_map, "lat", &str)) {
+            char *v = NULL;
+            latitude = strtod(str, &v);
+        }
+    }
     
-    Http_Request* httpblob =  Http_Parser_Parse((const char*)buffer);
-    if(httpblob == NULL){
-        response_string = "<h1>Invalid HTTP Request</h1>";
-        uint8_t outgoing_buffer[1024];
-        uint32_t outgoing_size = 0;
-        http_create_response(outgoing_buffer, sizeof(outgoing_buffer), response_string, strlen(response_string), &outgoing_size);
-
-        TCP_Server_Result send_result = tcp_server_send_to_client(server, client, outgoing_buffer, outgoing_size);
-
-        if(send_result != TCP_Server_Result_OK)
-        {
-            printf("Error on client send\n");
-
-        }
-        else{
-            printf("Send buffer content[0]: %u\n", client->outgoing_buffer[0]);
-        }
-        return;
-    }
-    printf("METHOD: [%s]\n", Http_Request_Get_Method_String(httpblob));
-    printf("PATH: [%s]\n", httpblob->start_line.path);
-
-    /*
-        char* response = route(httpblob->start_line.path, httpblob->start_line.method);
-    */
-   if(strcmp(Http_Request_Get_Method_String(httpblob), "OPTIONS") == 0){
-    printf("Hello from inside OPTIONS handling\n");
-        response_string = "<h1>OPTIONS</h1>";
-        uint8_t outgoing_buffer[1024];
-        uint32_t outgoing_size = 0;
-        http_create_response(outgoing_buffer, sizeof(outgoing_buffer), NULL, 0, &outgoing_size);
-        outgoing_size = strlen((char*)outgoing_buffer);
-        TCP_Server_Result send_result = tcp_server_send_to_client(server, client, outgoing_buffer, outgoing_size);
-
-        if(send_result != TCP_Server_Result_OK)
-        {
-            printf("Error on client send\n");
-
-        }
-        else{
-            /* printf("Send buffer content[0]: %u\n", client->outgoing_buffer[0]); */
-        }
-
-   }
-   else{
-        char* response;
-        /* Handle routing, TODO: add error handling etc*/
-        
-        handle_route(httpblob, &response);
-
-        printf("Generated response: %s\n", response);
-        
-        if(response == NULL)
-        {
-            response = "<h1>500 Server Error</h1>";
-        }
-        
-        uint8_t outgoing_buffer[1024];
-        uint32_t outgoing_size = 0;
-        http_create_response(outgoing_buffer, sizeof(outgoing_buffer), response, strlen(response), &outgoing_size);
-
-        if(response != NULL)
-        {
-            free(response);
-        }
-        
-        TCP_Server_Result send_result = tcp_server_send_to_client(server, client, outgoing_buffer, outgoing_size);
-
-        if (send_result != TCP_Server_Result_OK)
-        {
-            printf("Error on client send\n");
-        }
-        else
-        {
-            printf("Send buffer content[0]: %u\n", client->outgoing_buffer[0]);
+    double longitude = 0; {
+        const char *str = NULL;
+        if(http_query_map_get(query_map, "lon", &str)) {
+            char *v = NULL;
+            longitude = strtod(str, &v);
         }
     }
+    
+    if(longitude <= LONGITUDE_MIN) {
+        longitude = LONGITUDE_MIN + 1;
+    }
+    else if (longitude >= LONGITUDE_MAX) {
+        longitude = LONGITUDE_MAX - 1;
+    }
 
-    Http_Parser_Cleanup(httpblob);
+    if(latitude <= LATITUDE_MIN) {
+        latitude = LATITUDE_MIN;
+    }
+    else if (latitude >= LATITUDE_MAX) {
+        latitude = LATITUDE_MAX;
+    }
+    
+    printf("Longitude: %f, latitude: %f.\n", longitude, latitude);
 
+    Weather_Response weather_response;
+    memset(&weather_response, 0, sizeof(Weather_Response));
+    if(weather_get_data_double(longitude, latitude, &weather_response)) {
+        snprintf(
+            &out_response[0],
+            response_max_buffer_size,
+            "<html>"
+                "<head>"
+                    "<title>Weather</title>"
+                "</head>"
+                "<body>"
+                    "<h2>Latitude: %.2f, Longitude: %.2f</h2>"
+                    "<h3>Temperature (%c): %.2f (feels like %.2f)</h3>"
+                    "<a href=\"https://open-meteo.com/\">Weather data by Open-Meteo.com</a>"
+                "</body>"
+            "</html>"
+            ,
+            latitude,
+            longitude,
+            weather_response.unit,
+            weather_response.temperature,
+            weather_response.feels_like
+        );
+    }
+    else {
+        snprintf(
+            &out_response[0],
+            response_max_buffer_size,
+            "<html>"
+                "<head>"
+                    "<title>Weather</title>"
+                "</head>"
+                "<body>"
+                    "<h2>Failed to get a weather-report.</h2>"
+                "</body>"
+            "</html>"
+        );
+    }
+    
+    return true;
 }
 
 int main(int argc, char** argv) {
+    (void)argc;
+    (void)argv;
+
     printf("Hello, world! I am the Server.\n");
-    
-    Config_t* cfg = config_get_instance("settings.json");
 
-    if (cfg == NULL)
-    {
-        printf("Failed to load config. Error code: %d\n", config_instance_get_last_error());
-        return -1;
-    }
-    if(cfg->config_debug) 
-    {
-        printf("Debug mode is enabled.\n");
-        printf("config: \nserver_host: %s\nserver_port: %d\ndebug: %d\nmax_connections: %zu\nallowed_routes_count: %zu\nallowed_routes_route: %s\n",
-        cfg->config_server_host ? cfg->config_server_host : "NULL",
-        cfg->config_server_port,
-        cfg->config_debug,
-        cfg->config_max_connections,
-        cfg->allowed_routes_count,
-        cfg->allowed_routes[0].route);
-    }
-
-    if (argc >= 2)
-    {
-        printf("%i\n", argc);
-        printf("%s\n", argv[0]);
-        printf("%s\n", argv[1]);
-    }
-
-    TCP_Server server;
-    memset(&server, 0, sizeof(TCP_Server));
-
-    TCP_Server_Result server_init_result = tcp_server_init(
-        &server,
-        cfg->config_server_port,
-        &on_received_bytes_from_client
+    HTTP_Server http_server;
+    HTTP_Server_Result init_result = http_server_init(
+        &http_server,
+        8080 // TODO: SS - Get from argc.
     );
-    if(server_init_result != TCP_Server_Result_OK) 
-    {
-        printf("Failed to initialize TCP server. Result: %i.\n", server_init_result); // TODO: SS - tcp_server_get_result_as_string(server_init_result)
+    if(init_result != HTTP_Server_Result_OK) {
+        printf("Failed to start HTTP-server. Result: %i.\n", init_result);
         return -1;
-    }
+    } 
 
-    TCP_Server_Result start_server_result = tcp_server_start(&server);
-    if(start_server_result != TCP_Server_Result_OK) 
-    {
-        printf("Failed to start TCP server. Result: %i.\n", start_server_result); // TODO: SS - tcp_server_get_result_as_string(start_server_result)
-        return -1;
-    }
+    http_server_add_route(&http_server, "/", route_root);
+    http_server_add_route(&http_server, "/weather", route_weather);
 
     printf("Server running.\n");
 
-    
-
-    while(true) { // TEMP: SS - tcp_server_is_running(server)?
-
-        TCP_Server_Result work_result = tcp_server_work(&server);
-
-        switch(work_result){
-            case TCP_Server_Result_OK:
-            {
-                break;
-            }
-            default: 
-            {
-                printf("Something went wrong\n");
-                break;
-            }
-
-
+    while(true) {
+        HTTP_Server_Result work_result = http_server_work(&http_server);
+        if(work_result != HTTP_Server_Result_OK) {
+            printf("Failed to work the HTTP_Server. Got work-result %i.\n", work_result);
+            break;
         }
-
-        // TODO: SS - Tick the server.
-
-        // NOTE: SS - Sleep? Or get console-input here to be able to stop the server from the program.
+        
+        // NOTE: SS - Sleep?
     }
     
     printf("Server stopped.\n");
     
-    /* Dispose config on exit */
-    config_instance_dispose();
-    tcp_server_dispose(&server);
+    assert(http_server_dispose(&http_server) == HTTP_Server_Result_OK);
 
     return 0;
 }
