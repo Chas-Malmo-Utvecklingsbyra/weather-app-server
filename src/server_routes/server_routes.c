d0997007425e7d6aee0157fe3dc2be940ee703ca
Emilio <xxglobalelitexx123@gmail.com>
2025-12-06 12:08:25 +0100

    Fixed: a buffer overflow issue caused by strncpy

--- src/server_routes/server_routes.c
+++ src/server_routes/server_routes.c
@@ -193,6 +193,11 @@ int get_query_params(const char *path, const int max_params, char **keys, char *
             if (keys[number_of_params] == NULL)
                 return Route_Get_Params_Result_Error; /* Memory allocation failure, Server error */
             
+            if (i - key_index > MAX_PARAM_LENGTH)
+            {
+                return Route_Get_Params_Result_Malformed_Request;
+            }
+
             strncpy(keys[number_of_params], &path[key_index], i - key_index);
             keys[number_of_params][i - key_index] = '\0';
 
@@ -210,6 +215,11 @@ int get_query_params(const char *path, const int max_params, char **keys, char *
             if (!values[number_of_params])
             {
                 return Route_Get_Params_Result_Error; /* Memory allocation failure, Server error */
+            }
+
+            if (i - key_index > MAX_PARAM_LENGTH)
+            {
+                return Route_Get_Params_Result_Malformed_Request;
             }
 
             strncpy(values[number_of_params], &path[value_index], value_length);
